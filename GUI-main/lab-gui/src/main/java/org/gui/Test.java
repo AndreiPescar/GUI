2c56ee9d696528d53d9d8dc0395826f73f4f95c4
AndreiPescar <92308627+AndreiPescar@users.noreply.github.com>
2023-04-26 18:17:18 +0300

    Add files via upload

---
+++ GUI-main/lab-gui/src/main/java/org/gui/Test.java
@@ -0,0 +1,15 @@
+package org.gui;
+
+import org.gui.lab2.*;
+import org.gui.lab3.*;
+import org.gui.tema4.*;
+
+
+
+public class Test
+{
+    public static void main(String[] arguments)
+    {
+        new Chessboard();
+    }
+}
